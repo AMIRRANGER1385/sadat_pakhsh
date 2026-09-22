65a6e207797119c2d9848e2ecf2854093d0d6796
Amirmahdi <amh58311385@gmail.com>
2026-09-22 12:31:37 +0330

    Revert motions graphic

--- scripts/php-local-router.php
+++
@@ -1,7 +0,0 @@
-<?php
-declare(strict_types=1);
-$path=parse_url($_SERVER['REQUEST_URI']??'/',PHP_URL_PATH)?:'/';
-$file=realpath(__DIR__.'/../php-store/public_html'.$path);
-$root=realpath(__DIR__.'/../php-store/public_html');
-if($file&&str_starts_with($file,$root.DIRECTORY_SEPARATOR)&&is_file($file))return false;
-require __DIR__.'/../php-store/public_html/index.php';
